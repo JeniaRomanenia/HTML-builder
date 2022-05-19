93aa659e94c5cc766879d0dc4e54ff1ab78d07d9
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2022-05-19 20:50:55 +0300

    Update index.js
    add task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const strict = require('assert/strict');
+
+const { stdin, stdout } = process;
+filePath = path.join('02-write-file', 'text.txt')
+
+fs.writeFile(filePath, '', err => {
+    if(err) {
+        throw err;
+    }
+});
+
+stdout.write('Приветствую! Введите что нибудь...\n');
+stdin.on('data', data => {
+    let string = data.toString();
+    if (string.trim() === 'exit') {
+        stdout.write('Удачи!');
+        process.exit();
+    } else {
+        fs.appendFile (
+            filePath,
+            data,
+            err => {
+                if (err) {
+                    throw err;
+                }
+            }
+        );
+    }
+});
