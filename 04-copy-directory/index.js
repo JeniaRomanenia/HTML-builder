f62b95a7b91d31ec9807b4ab0e1cbff31de7348a
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2022-05-23 11:29:50 +0300

    Update index.js
    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+filesPath = path.join(__dirname, 'files');
+filesCopyPath = path.join(__dirname, 'files-copy');
+
+const copyDir = () => {
+    fs.readdir(filesPath, {withFileTypes: true})
+      .then(files => {
+        files.forEach(file => {
+          fs.copyFile(path.join(filesPath, file.name), path.join(filesCopyPath, file.name))
+            .catch(error => console.log(error));
+        });
+      })
+      .catch(error => console.log(error));
+  };
+  
+  //удаление папки если она уже создана (force:(true) исключения будут игнорироваться, recursion:(true) рекурсивное удаление папки)
+  fs.rm(filesCopyPath, {recursive: true, force: true})
+    .then(() => {
+      fs.mkdir(filesCopyPath)
+        .then(() => copyDir())
+        .catch(error => console.log(error));
+    });       
